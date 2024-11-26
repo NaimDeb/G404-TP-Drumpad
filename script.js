b4661cf7a80958defe55acd29f0c66f3cf821e04
naimdeb <naimdebard02@gmail.com>
2024-11-26 15:50:18 +0100

    added style and fixes

--- script.js
+++ script.js
@@ -3,138 +3,172 @@
 let recordList = [];
 let timeWhenRecordPressed = "";
 let latencyList = [];
+let isRecording = false
 
 // Prend les deux derniers boutons de la liste keyList au cas ou on change les boutons
 const recordButton = document.querySelector(`.record`)
 const playButton = document.querySelector(`.play`)
-
-let isRecording = false
-
+// const beatBoxButton = document.querySelector(`.beatBox`)
 
 
 document.addEventListener("keydown", handleKeyDown);
+// beatBoxButton.addEventListener("click", beatBox)
 
 
-// Quand on appuie sur une touche
+/**
+ * Fonction principale lancée quand on clique sur un bouton
+ * @param {*} event 
+ * @returns Nothing
+ */
 function handleKeyDown(event) {
+    // Evite répetitions non voulu
     if(event.repeat){return}
 
     let divSource = document.querySelector(`.key[data-key="${event.keyCode}"]`)
     
+    // Evite touche non voulue
     if (divSource == null) {return}
 
-    let audioSource
+    
         
-    // Si l'event
+    // Si touche est une touche qui fait du son
     if (event.keyCode != recordButton.dataset.key && event.keyCode != playButton.dataset.key){
 
-        audioSource = getAudioSource(event.keyCode);
+        let audioSource = getAudioSource(event.keyCode);
 
         playSound(audioSource);
         
         // Seulement quand ça record
         if (isRecording) {
 
-            recordList.push(audioSource)
+            // On garde dans liste recordList
+            recordList.push(event.keyCode)
 
-            // AAAAAAAAAAAAAAAAA
             addToLatency();
 
          }
 
-
-        divSource.classList.add("playing")
-
-        setTimeout(() => { 
-            divSource.classList.remove("playing");
-        }, 100 )
-
-
+        //  Animation
+        playAnimButton(divSource)
     }
 
     // Si key = R pour Record
     if (event.keyCode == "82") {
+        // Toggle isRecording, et lance la fonction nécessaire
         isRecording = !isRecording
         isRecording ? startRecord() : stopRecord()
     }
 
     // Si key = P pour Play
     if (event.keyCode == "80"){
+        // Lance fonction startPlaying seulement si RecordingFalse
         isRecording? null : startPlaying()
     }
 
 }
 
-
-
- function startRecord() {
+/**  
+ * 
+ * Initialise les arrays, la latence et l'animatoin pour le record
+*/
+function startRecord() {
     recordList = [];
     latencyList = [];
     recordButton.classList.add("playing")
 
 
     timeWhenRecordPressed = Date.now()
-    console.log("Recording starting " + timeWhenRecordPressed);
+    // console.log("Recording starting " + timeWhenRecordPressed);
     
 
 }
 
 
+/**
+ * Retire l'animation du bouton Record et garde la dernière latency de quand tu appuie sur le bouton
+ */ 
 function stopRecord() {
     recordButton.classList.remove("playing")
-    latencyList.push(Date.now() - timeWhenRecordPressed)
+
+    latencyList.push(Date.now() - timeWhenRecordPressed)    
 }
 
 
-
-
+/**
+ * Plays the recorded sounds with the latency
+ */
 function startPlaying(){
-
-    console.log(recordList);
-    console.log(latencyList);
-
 
     let latencyFinal = latencyList.map(record => record-latencyList[0])
 
-    console.log('latencyfinal');
-    
-    console.log(latencyFinal);
-    
+    playButton.classList.add("playing")
+
 
     for(let i = 0 ; i < recordList.length ; i++){
 
         setTimeout( () => {
-            playSound(recordList[i]);
-            console.log("play "+ recordList[i].src +" with latency " + latencyFinal[i]);
+            playAnimButton(getKeyDiv(recordList[i]));
+            playSound( getAudioSource(recordList[i]) );
+            // console.log("play "+ recordList[i].src +" with latency " + latencyFinal[i]);
             
         }, latencyFinal[i] )    
 
     }
+    setTimeout(() => {
+        playButton.classList.remove("playing")
+    }, latencyFinal[latencyFinal.length-1]);
      
 }
 
 function addToLatency(){    
     if (recordList.length > 1) {
         latencyList.push(Date.now() - timeWhenRecordPressed)
-        console.log("Added latency" );
         
 
-        console.log(Date.now() - timeWhenRecordPressed);
+        // console.log(Date.now() - timeWhenRecordPressed);
         
     }
 }
 
-
-// Fonction jouer son
+/**
+ * Joue le son donné avec .play()
+ * @param {*} son 
+ */
 function playSound(son) {
     son.currentTime = 0;
     son.play()
 }
 
-
-
-// Pour éviter trop de texte moche
+/**
+ * Récupère balise HTML audio avec data key donnée
+ * @param {*} keyCode 
+ * @returns htmlElement
+ */ 
 function getAudioSource(keyCode) {
     return document.querySelector("audio[data-key='" + keyCode + "']")
-
 }
+function getKeyDiv(keyCode) {
+    return document.querySelector(".key[data-key='" + keyCode + "']")
+}
+
+
+/**
+ * Plays the button animation
+ */
+async function playAnimButton(bouton){
+    try{
+    bouton.classList.add("playing")
+    setTimeout(() => { 
+        bouton.classList.remove("playing");
+    }, 300 )
+    }
+    catch(error) {console.log("marche po");
+    }
+}
+
+
+// TP 2
+
+// function beatBox() {
+//     beatBoxButton.classList.toggle("playing")
+// }
